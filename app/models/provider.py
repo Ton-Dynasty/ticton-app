4f1205aefa0131392f25ea7b27f1bb84e998b337
ipromise2324 <168allen55@gmail.com>
2024-02-04 16:34:37 +0800

    feat: add create_provider and get_provide_ by pair_id

--- app/models/provider.py
+++ app/models/provider.py
@@ -14,10 +14,14 @@ class Provider(BaseModel):
     id: str = Field(default_factory=lambda: uuid4().hex, description="Provider id")
     name: str = Field(description="Provider name")
     description: Optional[str] = Field(None, description="Provider description")
-    acc_rewards: float = Field(0.0, description="Accumulated reward tokens in human readable format")
+    acc_rewards: float = Field(
+        0.0, description="Accumulated reward tokens in human readable format"
+    )
     tick_amount: int = Field(0, description="Total tick counts")
     wind_amount: int = Field(0, description="Total wind counts")
-    pairs: List[Pair] = Field(description="List of pairs that provider is providing strategy for")
+    pairs: List[Pair] = Field(
+        description="List of pairs that provider is providing strategy for"
+    )
 
 
 class PriceResponse(BaseModel):
@@ -32,4 +36,24 @@ class PriceResponse(BaseModel):
 class CreateProviderRequest(BaseModel):
     name: str = Field(description="Provider name")
     description: Optional[str] = Field(None, description="Provider description")
-    pairs: List[Pair] = Field(description="List of pairs that provider is providing strategy for")
+    pairs: List[Pair] = Field(
+        description="List of pairs that provider is providing strategy for"
+    )
+
+    class Config:
+        populate_by_name = True
+        json_schema_extra = {
+            "example": {
+                "name": "Ton Dynasty",
+                "description": "Ton Dynasty is the official strategy provider for Tic Ton",
+                "pairs": [
+                    {
+                        "oracle_address": "kQDIuXyeKZ9-Bxezc2UaI6Ct8megUpIYwAjCIWOKPhkMMrip",
+                        "base_asset_address": "0QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACkT",
+                        "quote_asset_address": "kQBqSpvo4S87mX9tjHaG4zhYZeORhVhMapBJpnMZ64jhrP-A",
+                        "base_asset_symbol": "TON",
+                        "quote_asset_symbol": "jUSDT",
+                    },
+                ],
+            }
+        }
